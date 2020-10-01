b5a397d4a8a63207a0396bc16a1a985f7986be17
axios <axiosleo@foxmail.com>
2020-10-01 17:38:43 +0800

    init

---
+++ src/WorkermanServer.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace tpr\server;
+
+class WorkermanServer extends ServerHandler
+{
+    public function cgi(): void
+    {
+        // TODO: Implement cgi() method.
+    }
+
+    public function cli(string $command_name = null): void
+    {
+        // TODO: Implement cli() method.
+    }
+
+    protected function begin(): void
+    {
+        // TODO: Implement begin() method.
+    }
+
+    protected function end(): void
+    {
+        // TODO: Implement end() method.
+    }
+}
