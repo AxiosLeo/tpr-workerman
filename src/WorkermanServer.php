2875d92e0b101b302cb53c88e7e6954b8b4caf78
axios <axiosleo@foxmail.com>
2020-10-02 12:55:35 +0800

    handle workerman server

--- src/WorkermanServer.php
+++ src/WorkermanServer.php
@@ -2,25 +2,146 @@
 
 namespace tpr\server;
 
+use tpr\App;
+use tpr\Config;
+use tpr\Container;
+use tpr\core\Dispatch;
+use tpr\core\Response;
+use tpr\Event;
+use tpr\exception\HttpResponseException;
+use tpr\Files;
+use tpr\Path;
+use tpr\server\library\ConfigModel;
+use tpr\server\library\WorkermanRequest;
+use Workerman\Connection\ConnectionInterface;
+use Workerman\Protocols\Http\Request;
+use Workerman\Worker;
+
 class WorkermanServer extends ServerHandler
 {
-    public function cgi(): void
+    private ?Worker $worker = null;
+
+    private ?ConfigModel $config = null;
+
+    public function worker(): ?Worker
     {
-        // TODO: Implement cgi() method.
+        return $this->worker;
     }
 
-    public function cli(string $command_name = null): void
+    public function receive(ConnectionInterface $connection, Request $request)
     {
-        // TODO: Implement cli() method.
+        $req = new WorkermanRequest($request, $this->config);
+        Container::bindWithObj('request', $req);
+
+        try {
+            Container::dispatch()->run();
+        } catch (HttpResponseException $e) {
+            $this->send($connection, $e);
+        } catch (\Exception $e) {
+            try {
+                $response = Container::response();
+                if (App::debugMode()) {
+                    $response->response([
+                        'message' => $e->getMessage(),
+                        'code'    => $e->getCode(),
+                        'file'    => $e->getFile() . ':' . $e->getLine(),
+                        'trace'   => $e->getTraceAsString(),
+                    ], 500, 'server error');
+                } else {
+                    $response->response(['message' => $e->getMessage(), 'code' => $e->getCode()], 500, 'server error');
+                }
+            } catch (HttpResponseException $ex) {
+                $this->send($connection, $ex);
+            }
+        }
+        Container::delete('request');
+    }
+
+    protected function cgi(): void
+    {
+        throw new \Exception('CGI mode startup of the worker server is not supported.');
+    }
+
+    protected function cli(string $command_name = null): void
+    {
+        // init worker
+        $url                 = $this->config->protocol . '://' . $this->config->host . ':' . (string) ($this->config->port);
+        $this->worker        = new Worker($url, $this->config->context);
+        $this->worker->count = $this->config->worker;
+        if (!empty($this->config->options)) {
+            foreach ($this->config->options as $k => $v) {
+                $this->worker->{$k} = $v;
+            }
+        }
+
+        // clear cache
+        Files::remove(Path::cache());
+
+        Container::bind('response', Response::class);
+        $dispatch = new Dispatch($this->app->namespace);
+        Container::bindNXWithObj('cgi_dispatch', $dispatch);
+
+        $this->worker->onWorkerStart = function ($worker) {
+            Event::trigger('worker_start', $worker);
+        };
+
+        $this->worker->onWorkerReload = function ($worker) {
+            Event::trigger('worker_reload', $worker);
+        };
+
+        $this->worker->onConnect = function ($connection) {
+            Event::trigger('worker_connect', $connection);
+        };
+
+        $this->worker->onClose = function ($connection) {
+            Event::trigger('worker_close', $connection);
+        };
+
+        $this->worker->onBufferFull = function ($connection) {
+            Event::trigger('worker_buffer_full', $connection);
+        };
+
+        $this->worker->onBufferDrain = function ($connection) {
+            Event::trigger('worker_buffer_brain', $connection);
+        };
+
+        $this->worker->onError = function ($connection, $code, $msg) {
+            Event::trigger('worker_buffer_brain', $connection, $code, $msg);
+        };
+
+        Event::registerWithObj('worker_message', $this, 'receive');
+
+        // listen request
+        $this->worker->onMessage = function (ConnectionInterface $connection, Request $request) {
+            Event::trigger('worker_message', $connection, $request);
+        };
+
+        // run worker
+        Worker::runAll();
     }
 
     protected function begin(): void
     {
-        // TODO: Implement begin() method.
+        Event::trigger('app_begin', $this->app);
+        Config::load(Path::config());
+        $this->config = new ConfigModel();
+        if (!$this->app->server_options) {
+            $this->config->unmarshall($this->app->server_options);
+        }
+        $this->app->cache_time = 0;
     }
 
     protected function end(): void
     {
-        // TODO: Implement end() method.
+        Event::trigger('app_end', $this->app);
+    }
+
+    private function send(ConnectionInterface $connection, HttpResponseException $e)
+    {
+        $response = new \Workerman\Protocols\Http\Response();
+        $response->withHeaders($e->headers);
+        $response->withBody($e->result);
+        $response->withStatus($e->http_status);
+        $connection->send($response);
     }
 }
